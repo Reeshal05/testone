f09cfbd37bc3fe5e0ea38494635e6639673da72e
Reeshal05 <reeshaldsouza05@gmail.com>
2025-06-04 22:17:23 +0530

    first commit

---
+++ src/main/java/hudi/fry/App.java
@@ -0,0 +1,13 @@
+package hudi.fry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
